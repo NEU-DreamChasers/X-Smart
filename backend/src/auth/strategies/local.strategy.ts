253c536e5bd7a30f146412b92f2a8b307d28319b
“QuangNEU” <bebunguyen2222005@gmail.com>
2025-11-21 20:06:40 +0700

    feat:add authentication

---
+++ backend/src/auth/strategies/local.strategy.ts
@@ -0,0 +1,19 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+    constructor(private authService: AuthService) {
+        super(); // Mặc định nó sẽ lấy body field: "username" và "password"
+    }
+
+    async validate(username: string, pass: string): Promise<any> {
+        const user = await this.authService.validateUser(username, pass);
+        if (!user) {
+            throw new UnauthorizedException('Sai tên đăng nhập hoặc mật khẩu');
+        }
+        return user;
+    }
+}
---
+++ backend/src/users/user.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users') // Tên bảng trong DB
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    username: string;
+
+    @Column()
+    passwordHash: string;
+
+    @Column({ default: 'user' })
+    role: string;
+}
