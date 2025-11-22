10d3aaf7878d12463b19a5de683f3007e85ea57c
QuangNEU <bebunguyen2222005@gmail.com>
2025-11-22 17:21:19 +0700

    feat: authentication (#12)
    * config/update-dockerfile
    
    * chore:update-dependecies
    
    * chore:update dependencies
    
    * feat:add authentication
    
    ---------
    
    Co-authored-by: Luu Hieu <luuminhhieu1107@gmail.com>
    Co-authored-by: “QuangNEU” <“bebunguyen2222005@gmail.com>

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy, jwtConstants } from './strategies/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+
+@Module({
+    imports: [
+        UsersModule,
+        PassportModule,
+        JwtModule.register({
+            secret: jwtConstants.secret,
+            signOptions: { expiresIn: '60m' }, // Token sống 60 phút
+        }),
+    ],
+    controllers: [AuthController],
+    providers: [AuthService, LocalStrategy, JwtStrategy],
+    exports: [AuthService],
+})
+export class AuthModule { }
